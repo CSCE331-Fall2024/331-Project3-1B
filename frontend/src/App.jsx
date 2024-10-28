e245c2a97ce8c0da67c36f0c321eef452eb7bd3b
nthnbrks <nthnbrks@tamu.edu>
2024-10-28 18:46:16 -0500

    cleaned up color references + added path for menu board

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Customer from "./customer/customer.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ItemPage from "./customer/itemPage/itemPage.jsx";
+import MenuBoards from "./menuBoards/board1.jsx";
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Customer />} />
                 <Route path="/order" element={<ItemPage />} />
+                <Route path="/menuBoard" element={<MenuBoards />} />
             </Routes>
         </Router>
     );
--- frontend/src/menuBoards/board1.jsx
+++ frontend/src/menuBoards/board1.jsx
@@ -3,9 +3,11 @@ import "./menuBoards.css";
 export default function Board1() {
     return (
         <>
-            <div id="pick-a-meal">
-                <h1 id="pick-a-meal-header">PICK A MEAL</h1>
-                
+            <div id="board-container">
+                <div id="pick-a-meal">
+                    <h1 id="pick-a-meal-header">PICK A MEAL</h1>
+                    
+                </div>
             </div>
         </>
     );
