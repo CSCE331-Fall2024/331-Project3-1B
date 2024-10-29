e3e5702d85db8ce866bbbfa60e2f5f5ad06571b6
jasonthuynh <jasonhuynh512@gmail.com>
2024-10-28 19:16:19 -0500

    added greeting message

---
+++ frontend/src/manager/greetMessage/managerGreeting.jsx
@@ -0,0 +1,13 @@
+import "./managerGreeting.css";
+
+function ManagerGreeting() {
+    return (
+        <>
+            <div id="greeting-container">
+                <h1>Hello Manager!</h1>
+            </div>
+        </>
+    )
+}
+
+export default ManagerGreeting;
--- frontend/src/manager/manager.jsx
+++ frontend/src/manager/manager.jsx
@@ -1,9 +1,11 @@
+import ManagerGreeting from "./greetMessage/managerGreeting.jsx";
 import PageHeader from "./header/pageHeader.jsx";
 
 function manager() {
     return (
         <>
             <PageHeader />
+            <ManagerGreeting />
         </>
     )
 };
