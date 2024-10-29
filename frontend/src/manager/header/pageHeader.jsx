c574f25c8b0327b5b9dedda37e9618a92ea4e110
jasonthuynh <jasonhuynh512@gmail.com>
2024-10-28 19:07:26 -0500

    formatted text in buttons

--- frontend/src/manager/header/pageHeader.jsx
+++ frontend/src/manager/header/pageHeader.jsx
@@ -4,11 +4,25 @@ function PageHeader() {
     return (
         <>
             <div id="header-container">
-                <button>Inventory Orders</button>
-                <button>Employee Management</button>
-                <button>Sales Trends</button>
-                <button>Manage Items</button>
-                <button>Sales Order History</button>
+                <button className="nav-btn">
+                    <h1 className="nav-btn-text">Inventory Orders</h1>
+                </button>
+
+                <button className="nav-btn">
+                    <h1 className="nav-btn-text">Employee Management</h1>
+                </button>
+
+                <button className="nav-btn">
+                    <h1 className="nav-btn-text">Sales Trends</h1>
+                </button>
+
+                <button className="nav-btn">
+                    <h1 className="nav-btn-text">Menu Items</h1>
+                </button>
+
+                <button className="nav-btn">
+                    <h1 className="nav-btn-text">Sales Order History</h1>
+                </button>
             </div>
         </>
     )
