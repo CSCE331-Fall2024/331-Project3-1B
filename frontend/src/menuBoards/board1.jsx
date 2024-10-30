4e902297e9384b1a8cd041d1df8203dd5b197b01
nthnbrks <nthnbrks@tamu.edu>
2024-10-29 21:54:35 -0500

    added some menu board options

--- frontend/src/menuBoards/board1.jsx
+++ frontend/src/menuBoards/board1.jsx
@@ -5,8 +5,45 @@ export default function Board1() {
         <>
             <div id="board-container">
                 <div id="pick-a-meal">
+
                     <h1 id="pick-a-meal-header">PICK A MEAL</h1>
-                    
+
+                    <div id="options-list">
+
+                        <div className="option-generic">
+                            <span className="option-titles">BOWL</span>
+                            <span className="calories-count">240-1010</span>
+                            <span className="cal">cal</span>
+                            <span className="price">$6.70</span>
+                            <span className="starting-at">starting at*</span>
+
+                        </div>
+
+                        <div className="option-generic">
+                            <span className="option-titles">PLATE</span>
+                            <span className="calories-count">390-1500</span>
+                            <span className="cal">cal</span>
+                            <span className="price">$8.20</span>
+                            <span className="starting-at">starting at*</span>
+                        </div>
+
+                        <div className="option-generic">
+                            <span className="option-titles">BIGGER PLATE</span>
+                            <span className="calories-count">540-1990</span>
+                            <span className="cal">cal</span>
+                            <span className="price">$9.70</span>
+                            <span className="starting-at">starting at*</span>
+                        </div>
+
+                        <div className="option-generic">
+                            <span className="option-titles">PANDA CUB MEAL</span>
+                            <span className="calories-count">330-1020</span>
+                            <span className="cal">cal</span>
+                            <span className="price">$5.80</span>
+                            <span className="starting-at">starting at*</span>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </>
