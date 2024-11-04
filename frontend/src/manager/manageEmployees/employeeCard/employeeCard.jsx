da9be0345ec41a583449ea6a7b89b0854da5c3a7
jasonthuynh <jasonhuynh512@gmail.com>
2024-11-04 16:54:17 -0600

    added styling to employee cards

--- frontend/src/manager/manageEmployees/employeeCard/employeeCard.jsx
+++ frontend/src/manager/manageEmployees/employeeCard/employeeCard.jsx
@@ -5,9 +5,9 @@ function EmployeeCard({ name, position, email}) {
     return (
         <>
             <div id="employee-card">
-                <h2>{name}</h2>
-                <h2>{position}</h2>
-                <h2>{email}</h2>
+                <h2 className="employee-info">{name}</h2>
+                <h2 className="employee-info">{position}</h2>
+                <h2 className="employee-info">{email}</h2>
                 <button id="remove-employee-button">Remove Employee</button>
             </div>
         </>
