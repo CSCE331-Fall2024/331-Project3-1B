d5ef2f942b55a74239f6b4615bf9e7cb7a6f8164
jasonthuynh <jasonhuynh512@gmail.com>
2024-11-04 18:32:24 -0600

    added remove button functionality

--- frontend/src/manager/manageEmployees/employeeCard/employeeCard.jsx
+++ frontend/src/manager/manageEmployees/employeeCard/employeeCard.jsx
@@ -1,14 +1,14 @@
 import "./employeeCard.css";
 
 // creating individual employee cards
-function EmployeeCard({ name, position, email}) {
+function EmployeeCard({ name, position, email, removeEmployee }) {
     return (
         <>
             <div id="employee-card">
                 <h2 className="employee-info">{name}</h2>
                 <h2 className="employee-info">{position}</h2>
                 <h2 className="employee-info">{email}</h2>
-                <button id="remove-employee-button">Remove Employee</button>
+                <button id="remove-employee-button" onClick={ removeEmployee }>Remove Employee</button>
             </div>
         </>
     );
--- frontend/src/manager/manageEmployees/employeeCard/employeeContainer.jsx
+++ frontend/src/manager/manageEmployees/employeeCard/employeeContainer.jsx
@@ -1,18 +1,31 @@
+import React, { useState } from "react";
 import "./employeeContainer.css";
 import EmployeeCard from "./employeeCard.jsx";
 
 function EmployeeContainer() {
-
+    
     // temporary data
-    let employees = ["Jason", "Kaiqi", "Nick", "Zach", "Nathan"];
-    let positions = ["Cashier", "Manager", "Cashier", "Cashier", "Cashier"];
-    let emails = employees.map(employee => `${employee.toLowerCase()}@gmail.com`);
+    const initialEmployees = ["Jason", "Kaiqi", "Nick", "Zach", "Nathan"];
+    const initialPositions = ["Cashier", "Manager", "Cashier", "Cashier", "Cashier"];
+    const initialEmails = initialEmployees.map(employee => `${employee.toLowerCase()}@gmail.com`);
 
+    const [employees, setEmployees] = useState(initialEmployees);
+    const [positions, setPositions] = useState(initialPositions);
+    const [emails, setEmails] = useState(initialEmails);
+
+    console.log(initialEmployees);
+    function removeEmployee(index) {
+        setEmployees(employees.filter((_, i) => { return i != index }));
+        setPositions(positions.filter((_, i) => { return i != index }));
+        setEmails(emails.filter((_, i) => {return i != index }));
+    };
+    console.log(employees);
     return (
         <>
             <div id="employee-container">
+                <EmployeeCard name={"Email: "} position={"Position: "} email={"Name: "} />
                 {employees.map((employee, index) => {
-                    return <EmployeeCard name={employee} position={positions[index]} email={emails[index]} key={index} />
+                    return <EmployeeCard name={employee} position={positions[index]} email={emails[index]} key={index} removeEmployee = {() => removeEmployee(index)}/>
                 })}
             </div>
         </>
