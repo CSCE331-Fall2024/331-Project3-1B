1ab54c669e914ac664fc2652bebbb400553df206
jasonthuynh <jasonhuynh512@gmail.com>
2024-11-08 11:10:46 -0600

    added nodemon and added id field to manage employees view

--- frontend/src/manager/manageEmployees/employeeCard/employeeCard.jsx
+++ frontend/src/manager/manageEmployees/employeeCard/employeeCard.jsx
@@ -1,11 +1,12 @@
 import "./employeeCard.css";
 
 // creating individual employee cards
-function EmployeeCard({ name, position, email, removeEmployee }) {
+function EmployeeCard({ name, id, position, email, removeEmployee }) {
     return (
         <>
             <div id="employee-card">
                 <h2 className="employee-info">{name}</h2>
+                <h2 className="employee-info">{id}</h2>
                 <h2 className="employee-info">{position}</h2>
                 <h2 className="employee-info">{email}</h2>
                 <button id="remove-employee-button" onClick={ removeEmployee }>Remove Employee</button>
--- frontend/src/manager/manageEmployees/employeeCard/employeeContainer.jsx
+++ frontend/src/manager/manageEmployees/employeeCard/employeeContainer.jsx
@@ -6,16 +6,19 @@ function EmployeeContainer() {
     
     // temporary data
     const initialEmployees = ["Jason", "Kaiqi", "Nick", "Zach", "Nathan"];
+    const initialId = [1, 2, 3, 4, 5];
     const initialPositions = ["Cashier", "Manager", "Cashier", "Cashier", "Cashier"];
     const initialEmails = initialEmployees.map(employee => `${employee.toLowerCase()}@gmail.com`);
 
     const [employees, setEmployees] = useState(initialEmployees);
+    const [id, setId] = useState(initialId);
     const [positions, setPositions] = useState(initialPositions);
     const [emails, setEmails] = useState(initialEmails);
 
     console.log(initialEmployees);
     function removeEmployee(index) {
         setEmployees(employees.filter((_, i) => { return i != index }));
+        setId(id.filter((_, i) => { return i != index }));
         setPositions(positions.filter((_, i) => { return i != index }));
         setEmails(emails.filter((_, i) => {return i != index }));
     };
@@ -25,11 +28,12 @@ function EmployeeContainer() {
             <div id="employee-container">
                 <div id="employee-table-heading">
                     <h2>Name:</h2>
+                    <h2>ID:</h2>
                     <h2>Position:</h2>
                     <h2>Email:</h2>
                 </div>
                 {employees.map((employee, index) => {
-                    return <EmployeeCard name={employee} position={positions[index]} email={emails[index]} key={index} removeEmployee = {() => removeEmployee(index)}/>
+                    return <EmployeeCard name={employee} id={id[index]} position={positions[index]} email={emails[index]} key={index} removeEmployee = {() => removeEmployee(index)}/>
                 })}
             </div>
         </>
