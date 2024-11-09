5bcfcf7b4bc979245011328d13ebf2c50fc86c1a
jasonthuynh <jasonhuynh512@gmail.com>
2024-11-08 18:00:15 -0600

    added route to get 5 most recent orders of today

--- backend/routes/manager.js
+++ backend/routes/manager.js
@@ -20,6 +20,7 @@ router.get('/', (req, res) => {
     res.send('manager home');
 });
 
+// get list of all employee data
 router.get('/get_all_employees', (req, res) => {
     employees = []
     pool
@@ -33,6 +34,27 @@ router.get('/get_all_employees', (req, res) => {
         .catch(err => {
             console.error('Error executing query', err.stack);
             res.status(500).send("Error getting employees")
+        });
+});
+
+// get today's sales
+router.get('/sales_today', (req, res) => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Add leading zero
+    const day = String(currentDate.getDate()).padStart(2, '0');        // Add leading zero
+    const formattedDate = `${year}-${month}-${day}`;
+    const query = `SELECT * FROM sales_order_history 
+                    WHERE date_time_ordered BETWEEN '${formattedDate} 00:00:00' AND '${formattedDate} 23:59:59'
+                    ORDER BY date_time_ordered DESC LIMIT 5;`
+    pool
+        .query(query)
+        .then(query_res => {
+            res.json(query_res.rows);
+        })
+        .catch(err => {
+            console.error('Error getting most recent 5 orders', err.stack)
+            res.status(500).send('Error getting most recent 5 orders')
         });
 });
 
