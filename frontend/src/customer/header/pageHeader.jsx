6e3fde1252630f18d888ffb8393e541d3f0d1cc9
NicholasMatias <n-r-matias@outlook.com>
2024-11-13 16:29:24 -0600

    used window href to determine if the my bag button should exist on cashier page when ordering => it shouldn't
    cashier page => done.

--- frontend/src/customer/header/pageHeader.jsx
+++ frontend/src/customer/header/pageHeader.jsx
@@ -1,11 +1,11 @@
 import "./pageHeader.css";
 import { useState } from "react";
-import MyBag from "../myBag/myBag.jsx";
 import { useNavigate } from "react-router-dom";
 
 // PageHeader component, includes myBag component => allows user to view what is in their bag.
 export default function PageHeader() {
-    
+    let currURL = window.location.href;
+
     const navigate = useNavigate();
 
     // State to show or hide the MyBag component
@@ -14,18 +14,20 @@ export default function PageHeader() {
     // Function to toggle the myBag component be displayed or not
     const goToMyBag = () => {
         navigate("/myBag");
-    }  
+    };
 
     return (
         <>
             <div id="header-container">
                 <h1 id="header-title">Panda Express</h1>
-                <button onClick={goToMyBag} id="header-button">
-                    <h1 className="header-button-title">My Bag</h1>
-                </button>
+                {currURL.includes("cashier") ? (
+                    <></>
+                ) : (
+                    <button onClick={goToMyBag} id="header-button">
+                        <h1 className="header-button-title">My Bag</h1>
+                    </button>
+                )}
             </div>
-            
         </>
     );
 }
-
