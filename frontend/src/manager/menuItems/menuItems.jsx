dc58f7953be1c6f3c7be4ab15014b77354aa3057
jasonthuynh <jasonhuynh512@gmail.com>
2024-11-13 23:09:48 -0600

    added menuItem page in manager view

--- frontend/src/manager/menuItems/menuItems.jsx
+++ frontend/src/manager/menuItems/menuItems.jsx
@@ -1,10 +1,37 @@
+import { useState, useEffect } from 'react';
 import './menuItems.css';
 import PageHeader from '../header/pageHeader.jsx';
+import SingleMenuItem from './singleMenuItem.jsx';
 
 function MenuItems() {
+    const [itemId, setItemID] = useState([]);
+    const [itemName, setItemName] = useState([]);
+    const [itemType, setItemType] = useState([]);
+    const [itemAvailability, setItemAvailability] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/manager/get_menu_items')
+            .then(response => response.json())
+            .then(items => {
+                const itemIds = items.map(item => item.item_serial_number);
+                const itemNames = items.map(item => item.item_name);
+                const itemTypes = items.map(item => item.item_type);
+                const itemAvailabilities = items.map(item => item.availability);
+                setItemID(itemIds);
+                setItemName(itemNames);
+                setItemType(itemTypes);
+                setItemAvailability(itemAvailabilities);
+            })
+            .catch(error => {
+                console.error('Error getting menu items');
+            })
+    }, []);
     return(
         <>
             <PageHeader />
+            <div id='menu-item-container'>
+                {itemId.map((id, index) => <SingleMenuItem key={index} item_id={id} item_name={itemName[index]} item_type={itemType[index]} item_availability={itemAvailability[index]} />)}
+            </div>
         </>
     );
 };
---
+++ frontend/src/manager/menuItems/singleMenuItem.jsx
@@ -0,0 +1,15 @@
+import './singleMenuItem.css';
+
+
+function SingleMenuItem({item_id, item_name, item_type, item_availability}) {
+    return (
+        <div id='item-container'>
+            <h3>{item_id}</h3>
+            <h3>{item_name}</h3>
+            <h3>{item_type}</h3>
+            <h3>{item_availability}</h3>
+        </div>
+    );
+};
+
+export default SingleMenuItem;
