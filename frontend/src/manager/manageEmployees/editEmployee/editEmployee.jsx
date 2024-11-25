b465b9740b04faa13f72f2af6c53e0eee2f5c6ac
kaiqi-eng <66494010+kaiqi-eng@users.noreply.github.com>
2024-11-24 21:12:11 -0600

    Update editEmployee.jsx

--- frontend/src/manager/manageEmployees/editEmployee/editEmployee.jsx
+++ frontend/src/manager/manageEmployees/editEmployee/editEmployee.jsx
@@ -1,6 +1,9 @@
 import './editEmployee.css';
 import PageHeader from '../../header/pageHeader';
-
+/** 
+ * generates the component to edit employee attributes
+ * @return {HTML} EditEmployee
+ */
 function EditEmployee() {
     return <>
         <PageHeader />
